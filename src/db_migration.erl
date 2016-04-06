3b47b4f3c618c03b539a993c90a18ff45017ad22
gaurav kumar <gauravkumar552@gmail.com>
2016-04-06 15:26:49 +0530

    Better structure, Removes hardcoding and general cleanup

--- src/db_migration.erl
+++ src/db_migration.erl
@@ -1,22 +1,22 @@
+%% @author Gaurav Kumar <gauravkumar552@gmail.com>
+
 -module(db_migration).
+
 -export([start_mnesia/0, transform/2, get_base_revision/0,
 	 create_migration_file/1, init_migrations/0,
 	 get_next_revision/1, get_current_head/1, get_current_head/0,
 	 read_config/0, create_migration_file/0,
 	 find_pending_migrations/0, apply_upgrades/0,
 	 get_revision_tree/0, append_revision_tree/2,
-	 get_applied_head/0, update_head/1, has_migration_behaviour/1
+	 get_applied_head/0, update_head/1
 	]).
 
--define(URNAME, user).
 -define(TABLE, schema_migrations).
--define(BaseDir, "/home/gaurav/project/mnesia_migrate/migrations/").
--define(ProjDir, "/home/gaurav/project/mnesia_migrate/").
 
 -record(schema_migrations, {curr_head=null, extra_info=null}).
 
 read_config() ->
-    Val = case application:get_env(mnesia_migrate, migration_dir, "~/project/mnesia_migrate/migrations/") of
+    Val = case application:get_env(mnesia_migrate, migration_dir, "~/project/mnesia_migrate/src/migrations/") of
         {ok, Value} -> Value;
         Def -> Def
 	end,
@@ -49,93 +49,13 @@ transform(_Old_struct, _New_struct) ->
 %    io:fwrite("Tables needing migration : ~p~n", [Tabletomigrate]),
 %    Tabletomigrate.
 
-
-has_migration_behaviour(Modulename) ->
-    case catch Modulename:module_info(attributes) of
-        {'EXIT', {undef, _}} ->
-            false;
-        Attributes ->
-            case lists:keyfind(behaviour, 1, Attributes) of
-                {behaviour, BehaviourList} ->
-                    lists:member(migration, BehaviourList);
-                false ->
-                    false
-            end
-    end.
-
-
-get_base_revision() ->
-    Modulelist = filelib:wildcard("ebin/*.beam"),
-    Res = lists:filter(fun(Filename) ->
-        Modulename = list_to_atom(filename:basename(Filename, ".beam")),
-        case has_migration_behaviour(Modulename) of
-	    true -> string:equal(Modulename:get_prev_rev(),none);
-	    false -> false
-	end
-    end,
-    Modulelist),
-    BaseModuleName = list_to_atom(filename:basename(Res, ".beam")),
-    io:fwrite("Base Rev file is ~p~n", [BaseModuleName]),
-    case Res of
-        [] -> none;
-	_ -> BaseModuleName:get_current_rev()
-    end.
-
-
-get_next_revision(RevId) ->
-    Modulelist = filelib:wildcard("migrations/*.beam"),
-    Res = lists:filter(fun(Filename) ->
-        Modulename = list_to_atom(filename:basename(Filename, ".beam")),
-        OldrevId = Modulename:get_prev_rev(),
-        string:equal(OldrevId, RevId)
-    end,
-    Modulelist),
-    %io:fwrite("Base Rev: ~p Next Rev ~p~n", [RevId,Res]),
-    case Res of
-    [] -> [];
-    _ -> ModuleName = list_to_atom(filename:basename(Res, ".beam")), ModuleName:get_current_rev()
-    end.
-
-get_current_head(RevId) ->
-    case get_next_revision(RevId) of
-	[] -> RevId ;
-        NextRevId -> get_current_head(NextRevId)
-    end.
+%%
+%%Functions related to migration info
+%%
 
 get_current_head() ->
     BaseRev = get_base_revision(),
     get_current_head(BaseRev).
-
-create_migration_file(CommitMessage) ->
-    erlydtl:compile('schema.template', migration_template),
-    NewRevisionId = string:substr(uuid:to_string(uuid:uuid4()),1,8),
-    OldRevisionId = get_current_head(),
-    Filename = NewRevisionId ++ "_" ++ string:substr(CommitMessage, 1, 20) ,
-    {ok, Data} = migration_template:render([{new_rev_id , NewRevisionId}, {old_rev_id, OldRevisionId},
-					  {modulename, Filename}, {tabtomig, []},
-					  {commitmessage, CommitMessage}]),
-    FilePath = case application:get_env(mnesia_migrate, migration_dir, "/home/gaurav/project/mnesia_migrate/src/migrations/") of
-        {ok, Value} -> Value;
-        Def -> Def
-	end,
-    file:write_file(FilePath ++ Filename ++ ".erl", Data),
-    io:format("New file created ~p~n", [Filename ++ ".erl"]).
-
-create_migration_file() ->
-    erlydtl:compile('schema.template', migration_template),
-    NewRevisionId = "a" ++ string:substr(uuid:to_string(uuid:uuid4()),1,8),
-    BaseRev = get_base_revision(),
-    OldRevisionId = get_current_head(BaseRev),
-    Filename = NewRevisionId ++ "_migration" ,
-    {ok, Data} = migration_template:render([{new_rev_id , NewRevisionId}, {old_rev_id, OldRevisionId},
-					  {modulename, Filename}, {tabtomig, []},
-					  {commitmessage, "migration"}]),
-    FilePath = case application:get_env(mnesia_migrate, migration_dir, "/home/gaurav/project/mnesia_migrate/src/migrations/") of
-        {ok, Value} -> Value;
-        Def -> Def
-	end,
-    file:write_file(FilePath ++ Filename ++ ".erl", Data),
-    io:format("New file created ~p~n", [Filename ++ ".erl"]).
 
 get_revision_tree() ->
     BaseRev = get_base_revision(),
@@ -145,15 +65,45 @@ get_revision_tree() ->
     RevList.
 
 find_pending_migrations() ->
-   % fetch current revision head from database
    AppliedHead = case get_applied_head() of
-   none -> get_base_revision() ;
-   Id -> Id
-   end,
+                     none -> get_base_revision() ;
+                     Id -> Id
+                 end,
    List1 = [],
    RevList = append_revision_tree(List1, AppliedHead),
    io:format("Revisions needing migration : ~p~n", [RevList]),
    RevList.
+
+%%
+%% Functions related to migration creation
+%%
+
+create_migration_file(CommitMessage) ->
+    erlydtl:compile('schema.template', migration_template),
+    NewRevisionId = string:substr(uuid:to_string(uuid:uuid4()),1,8),
+    OldRevisionId = get_current_head(),
+    Filename = NewRevisionId ++ "_" ++ string:substr(CommitMessage, 1, 20) ,
+    {ok, Data} = migration_template:render([{new_rev_id , NewRevisionId}, {old_rev_id, OldRevisionId},
+					  {modulename, Filename}, {tabtomig, []},
+					  {commitmessage, CommitMessage}]),
+    file:write_file(get_migration_source_filepath() ++ Filename ++ ".erl", Data),
+    io:format("New file created ~p~n", [Filename ++ ".erl"]).
+
+create_migration_file() ->
+    erlydtl:compile('schema.template', migration_template),
+    NewRevisionId = "a" ++ string:substr(uuid:to_string(uuid:uuid4()),1,8),
+    OldRevisionId = get_current_head(),
+    Filename = NewRevisionId ++ "_migration" ,
+    {ok, Data} = migration_template:render([{new_rev_id , NewRevisionId}, {old_rev_id, OldRevisionId},
+					  {modulename, Filename}, {tabtomig, []},
+					  {commitmessage, "migration"}]),
+    file:write_file(get_migration_source_filepath() ++ Filename ++ ".erl", Data),
+    io:format("New file created ~p~n", [Filename ++ ".erl"]).
+
+
+%%
+%% Functions related to applying migrations
+%%
 
 apply_upgrades() ->
     RevList = find_pending_migrations(),
@@ -173,8 +123,82 @@ append_revision_tree(List1, RevId) ->
 
 get_applied_head() ->
 	{atomic, KeyList} = mnesia:transaction(fun() -> mnesia:all_keys(schema_migrations) end),
-	io:format("current applied head is : ~p~n", [hd(KeyList)]),
-	hd(KeyList).
+	Head = case length(KeyList) of
+	           0 -> none;
+	           _ -> hd(KeyList)
+               end,
+	io:format("current applied head is : ~p~n", [Head]),
+	Head.
 
 update_head(Head) ->
 	mnesia:transaction(fun() -> mnesia:write(schema_migrations, #schema_migrations{curr_head = Head}, write) end).
+
+%%
+%% helper functions
+%%
+
+has_migration_behaviour(Modulename) ->
+    case catch Modulename:module_info(attributes) of
+        {'EXIT', {undef, _}} ->
+            false;
+        Attributes ->
+            case lists:keyfind(behaviour, 1, Attributes) of
+                {behaviour, BehaviourList} ->
+                    lists:member(migration, BehaviourList);
+                false ->
+                    false
+            end
+    end.
+
+
+get_base_revision() ->
+    Modulelist = filelib:wildcard(get_migration_beam_filepath() ++ "*_migration.beam"),
+    Res = lists:filter(fun(Filename) ->
+        Modulename = list_to_atom(filename:basename(Filename, ".beam")),
+        case has_migration_behaviour(Modulename) of
+	    true -> string:equal(Modulename:get_prev_rev(),none);
+	    false -> false
+	end
+    end,
+    Modulelist),
+    BaseModuleName = list_to_atom(filename:basename(Res, ".beam")),
+    io:fwrite("Base Rev module is ~p~n", [BaseModuleName]),
+    case Res of
+        [] -> none;
+	_ -> BaseModuleName:get_current_rev()
+    end.
+
+
+get_next_revision(RevId) ->
+    Modulelist = filelib:wildcard(get_migration_beam_filepath() ++ "*_migration.beam"),
+    Res = lists:filter(fun(Filename) ->
+        Modulename = list_to_atom(filename:basename(Filename, ".beam")),
+        case has_migration_behaviour(Modulename) of
+	    true -> string:equal(Modulename:get_prev_rev(), RevId);
+	    false -> false
+	end
+    end,
+    Modulelist),
+    %io:fwrite("Base Rev: ~p Next Rev ~p~n", [RevId,Res]),
+    case Res of
+    [] -> [];
+    _ -> ModuleName = list_to_atom(filename:basename(Res, ".beam")), ModuleName:get_current_rev()
+    end.
+
+get_current_head(RevId) ->
+    case get_next_revision(RevId) of
+	[] -> RevId ;
+        NextRevId -> get_current_head(NextRevId)
+    end.
+
+get_migration_source_filepath() ->
+    case application:get_env(mnesia_migrate, migration_source_dir, "/home/gaurav/project/mnesia_migrate/src/migrations/") of
+        {ok, Value} -> Value;
+        Def -> Def
+    end.
+
+get_migration_beam_filepath() ->
+    case application:get_env(mnesia_migrate, migration_beam_dir, "/home/gaurav/project/mnesia_migrate/ebin/") of
+        {ok, Value} -> Value;
+        Def -> Def
+    end.
