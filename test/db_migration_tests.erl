89ed6a8d461f70057e463f5fa21803ac74e29f15
Srijan Choudhary <srijan4@gmail.com>
2018-03-27 20:50:31 +0530

    Add better tests

--- test/db_migration_tests.erl
+++ test/db_migration_tests.erl
@@ -45,64 +45,58 @@ check_migration_file_creation_test_() ->
 	 }
 	].
 
-check_database_related_function_test_() ->
-	[
-	 {"check_applied_head_test_1",
-	  fun() ->
-	      mnesia:create_schema([node()]),
-	      db_migration:start_mnesia(),
-	      db_migration:init_migrations(),
-	      ?assertEqual(true, is_atom(db_migration:get_applied_head()))
-	  end
-	 }
-	].
+mnesia_migration_test_() ->
+    {
+     setup,
+     fun() ->
+             start_mnesia()
+     end,
+     fun(_) ->
+             stop_mnesia()
+     end,
+     [
+      {inorder,
+       [{"Init Migrations should not crash",
+         ?_assertEqual(ok, db_migration:init_migrations())},
+        {
+         "Test migration calculation functions",
+         [{inparallel,
+           [
+            ?_assertEqual(none, db_migration:get_applied_head()),
+            ?_assertEqual(test1, db_migration:get_base_revision()),
+            ?_assertEqual(test2, db_migration:get_next_revision(test1)),
+            ?_assertEqual(test1, db_migration:get_prev_revision(test2)),
+            ?_assertEqual([test1, test2],
+                          db_migration:find_pending_migrations()),
+            ?_assertEqual(1, db_migration:get_count_between_2_revisions(
+                               test1, test2))
+           ]
+          }]
+        },
+        {"Test apply upgrades",
+         [?_assertEqual({ok, applied}, db_migration:apply_upgrades()),
+          ?_assertEqual(test2, db_migration:get_applied_head())]
+        },
+        {"Test apply downgrades",
+         [?_assertEqual({error, wrong_number},
+                        db_migration:apply_downgrades(5)),
+          ?_assertEqual(ok, db_migration:apply_downgrades(1)),
+          ?_assertEqual(test1, db_migration:get_applied_head())
+         ]}
+       ]}]
+    }.
 
-check_db_related_test_() ->
-	[
-	 {"check_applied_head_test_2",
-	  fun() ->
-	      mnesia:create_schema([node()]),
-	      db_migration:start_mnesia(),
-	      db_migration:init_migrations(),
-	      ?assertEqual(false, is_list(db_migration:get_applied_head()))
-	  end
-	 }
-	].
+%% Internal functions
 
-check_single_upgrade_function_test_() ->
-	[
-	 {
-	 "check basic upgrade",
-	  fun () ->
-	      db_migration:start_mnesia(),
-	      {ok, Mod} = compile:file('test/test_migration_1'),
-	      Mod:down(),
-	      Resp = Mod:up(),
-	      Mod:down(),
-	      ?assertEqual(test_val, Resp)
-	  end
-	 }
-	].
+start_mnesia() ->
+    ok = mnesia:delete_schema([node()]),
+    ok = mnesia:create_schema([node()]),
+    {ok, [mnesia]} = application:ensure_all_started(mnesia),
+    application:ensure_all_started(mnesia_migrate),
+    EbinDir = code:lib_dir(mnesia_migrate) ++ "/test/",
+    application:set_env(mnesia_migrate, migration_beam_dir, EbinDir).
 
-check_consecutive_upgrade_function_test_() ->
-	[
-	 {
-	 "check consecutive upgrade",
-	  fun () ->
-	      db_migration:start_mnesia(),
-	      {ok, Mod} = compile:file('test/test_migration_1'),
-	      Mod:up(),
-	      {ok, Mod1} = compile:file('test/test_migration_2'),
-	      UpResp = Mod1:up(),
-	      ?assertEqual({test_table,test_val,null,whoa}, UpResp),
-	      os:cmd("sed -i -e 's/upgrade/downgrade/g' test/test_migration_2.erl"),
-	      os:cmd("rm test/test_migration_2.beam"),
-	      {ok, Mod2} = compile:file('test/test_migration_2.erl'),
-	      code:load_file(Mod2),
-	      os:cmd("sed -i -e 's/downgrade/upgrade/g' test/test_migration_2.erl"),
-	      DownResp = Mod2:down(),
-	      ?assertEqual({test_table,test_val,null}, DownResp),
-	      Mod:down()
-	  end
-	 }
-	].
+stop_mnesia() ->
+    ok = application:stop(mnesia_migrate),
+    ok = application:stop(mnesia),
+    ok = mnesia:delete_schema([node()]).
